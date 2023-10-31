4b5492eb5ef635636ab7ae9993f75c59fe1c986c
Gbot <48814389+xGbot@users.noreply.github.com>
2023-10-30 22:03:29 -0400

    testing

--- app/src/main/java/com/example/locationpinnedapp/MainActivity.java
+++ app/src/main/java/com/example/locationpinnedapp/MainActivity.java
@@ -2,8 +2,16 @@ package com.example.locationpinnedapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +21,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button search = findViewById(R.id.button_search);
+        TextView result = findViewById(R.id.textview_result);
+        EditText lat = findViewById(R.id.edittext_latitude);
+        EditText lon = findViewById(R.id.edittext_longitude);
+
+        if (Geocoder.isPresent()) {
+            Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+            String address = "900 McGill Road";
+            try {
+                List<Address> ls= geocoder.getFromLocationName(address, 5);
+                for (Address addr: ls) {
+                    double latitude = addr.getLatitude();
+                    double longitude = addr.getLongitude();
+                    System.out.println(addr);
+                }
+            } catch (IOException e) {
+
+            }
+        }
 
 
 
